6dc5301b739b44704670cc1a839da0d9fcd9be97
Alberto <atriguerosholgado@gmail.com>
2025-11-19 16:19:14 +0100

    se añade carpeta clean

--- src/load.py
+++ src/load.py
@@ -5,7 +5,7 @@ Funcion para cargar el dataframe en un csv
 """
 def df_to_csv(df):
     try:
-        df.to_csv("data/world_happiness_clean.csv")
+        df.to_csv("data/clean/world_happiness_clean.csv")
         print("SE HA CREADO EL FICHERO world_happiness_clean.csv")
     except Exception as e:
         print(f"ERROR N SE HA PODIDO CREAR EL FICHERO world_happiness_clean.csv: {e}")
